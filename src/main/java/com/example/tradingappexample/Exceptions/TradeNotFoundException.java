638f94f8f99f71580a8a42be761d666ed18e0508
DanielProkopDev <102687908+DanielProkopDev@users.noreply.github.com>
2025-12-16 19:22:08 +0100

    Delete src/main/java/com/example/tradingappexample/Exceptions directory

--- src/main/java/com/example/tradingappexample/Exceptions/TradeNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.tradingappexample.Exceptions;
-
-import java.util.UUID;
-
-public class TradeNotFoundException extends RuntimeException{
-    public TradeNotFoundException(UUID id){
-        super("Trade with id " + id + " not found");
-    }
-}
